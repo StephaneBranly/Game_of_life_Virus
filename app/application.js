482f2caf26ef66869471b18710960ecb7fd67403
StephaneBranly <stephanebranly.pro@gmail.com>
2020-02-29 22:29:41 +0100

    add: planisphere functions

--- app/application.js
+++ app/application.js
@@ -1,19 +1,44 @@
 function createGeneration(width, height) {
   var generation = new Array(height);
-
+  var planisphere = calculatePlanisphere(width, height);
+  alert("Fin de calcul planisphere");
   for (var y = 0; y < height; y++) {
     generation[y] = [];
     for (var x = 0; x < width; x++) {
-      generation[y][x] = Math.floor(Math.random() * 2);
+      if (planisphere[y][x] == 1) {
+        generation[y][x] = Math.floor(Math.random() * 2);
+      } else {
+        generation[y][x] = 0;
+      }
     }
   }
   return generation;
 }
 
+function calculatePlanisphere(width, height) {
+  alert("Calcul planisphere");
+  alert("Green pixel");
+  var planisphere = new Array(height);
+  var canvas_terrain = document.getElementById("canvas_terrain");
+  var ctx_terrain = canvas_terrain.getContext("2d");
+  for (var y = 0; y < height; y++) {
+    planisphere[y] = [];
+    for (var x = 0; x < width; x++) {
+      if (ctx_terrain.getImageData(x, y, 1, 1).data[1] == 255) {
+        planisphere[y][x] = 1;
+      } else {
+        planisphere[y][x] = 0;
+      }
+    }
+  }
+
+  return planisphere;
+}
+
 function draw(context2d, generation, totalGeneration) {
   var height = generation.length;
   var width = generation[0].length;
-  var scale = 2;
+  var scale = 1;
 
   console.log(totalGeneration);
   document.getElementById("generation_id").innerHTML = totalGeneration;
@@ -30,6 +55,17 @@ function clearBackground(context2d, width, height, scale) {
   context2d.fillStyle = "red";
 
   context2d.fillRect(0, 0, 4, 4);
+}
+
+function drawPlanisphere(context2d, width, height, scale) {
+  context2d.fillStyle = "green";
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      if (generation[y][x] === 1) {
+        context2d.fillRect(x * scale, y * scale, scale, scale);
+      }
+    }
+  }
 }
 
 function drawCells(context2d, generation, width, height, scale) {
@@ -111,10 +147,19 @@ function update(ctx, generation, totalGeneration) {
 }
 
 (function() {
-  var generation = createGeneration(400, 200);
-  var c = document.getElementById("canvas");
-  var ctx = c.getContext("2d");
-  var totalGeneration = 100;
+  var img = new Image();
+  img.crossOrigin = "anonymous";
+  img.src = "./ressources/planisphere_terrain.png";
+  var canvas_terrain = document.getElementById("canvas_terrain");
+  var ctx_terrain = canvas_terrain.getContext("2d");
+  img.onload = function() {
+    ctx_terrain.drawImage(img, 0, 0);
 
-  draw(ctx, generation, totalGeneration);
+    var generation = createGeneration(1282, 641);
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    var totalGeneration = 0;
+
+    draw(ctx, generation, totalGeneration);
+  };
 })();
